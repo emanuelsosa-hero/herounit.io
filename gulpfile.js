a5e6fc6b489c803d476537ab27e792d8f612acd8
Rafael <rafanetow@gmail.com>
2018-10-11 16:14:57 -0700

    bower component changed to node modules

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,23 @@ var sassPaths = [
   'node_modules/foundation-sites/scss',
   'node_modules/motion-ui/src'
 ];
-
+/*
+script(src='../bower_components/jquery/dist/jquery.js')
+script(src='../bower_components/what-input/dist/what-input.js')
+script(src='../node_modules/slick-carousel/slick/slick.min.js')
+script(src='../bower_components/foundation-sites/dist/js/foundation.js')
+script(src='../bower_components/foundation-sites/dist/js/plugins/foundation.equalizer.js')
+script(src='../js/app.js')
+*/
 const jsFiles = [
   'node_modules/jquery/dist/jquery.js',
   'node_modules/feather-icons/dist/feather.js',
-  './src/js/**/*.js'
+  './src/js/**/*.js',
+  'node_moudles/jquery/dist/jquery.js',
+  'node_modules/what-input/dist/what-input.js',
+  'node_modules/slick-carousel/slick/slick.min.js',
+  'node_modules/foundation-sites/dist/js/foundation.js',
+  'node_modules/foundation-sites/dist/js/plugins/foundation.equalizer.js'
 ];
 
 const cssFiles = [
